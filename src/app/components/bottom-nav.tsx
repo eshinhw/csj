1209b9b8ca38e2d60f09f3db2323e460ad5018e3
Eddie Shin <eshinhw@gmail.com>
2023-04-02 16:14:56 -0400

    removed special characters

--- src/app/components/bottom-nav.tsx
+++ src/app/components/bottom-nav.tsx
@@ -27,7 +27,6 @@ export default function BottomNav() {
             <div>
               <div className="uppercase font-bold">OPINION</div>
               <div className="flex flex-col text-sm">
-                <a>Today's Opinion</a>
                 <a>Columnists</a>
                 <a>Editorials</a>
                 <a>Letters</a>
@@ -37,7 +36,7 @@ export default function BottomNav() {
               <div className="uppercase font-bold">ARTS</div>
               <div className="flex flex-col text-sm">
                 <a>Today's Arts</a>
-                <a>Arts & Design</a>
+      
                 <a>Books</a>
                 <a>Music</a>
                 <a>Movies</a>
