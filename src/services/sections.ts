8ef208db5003a4ea6e7f9846d234476c5d0feb77
Eddie Shin <eshinhw@gmail.com>
2023-04-02 15:12:52 -0400

    updated subsections

--- src/services/sections.ts
+++ src/services/sections.ts
@@ -1,1 +1,23 @@
-export function getSectionData() {}
+const sectionData:sectionDataType = {
+  world: ["AFRICA", "AMERICAS", "ASIA", "AUSTRALIA", "EUROPE", "MIDDLE EAST"],
+  canada: ["Alberta", "British Columbia", "Ontario"],
+  food: ["Restaurants", "Cafes", "Bars"],
+  beauty: ["Hair Care", "Skin Care", "Fitness"],
+  realty: ["Residential", "Commercial", "Sales", "Rent"],
+  promotion: ["Dining Festival", "Groceries Deals", "Electronics Discount"],
+  opinion: ["Trending", "Essays", "Columnists"],
+};
+
+type sectionDataType = {
+  [world:string]:string[],
+  canada:string[],
+  food:string[],
+  beauty:string[],
+  realty:string[],
+  promotion:string[],
+  opinion:string[]
+}
+
+export function getSectionData(section: string): string[] {
+  return sectionData[section];
+}
