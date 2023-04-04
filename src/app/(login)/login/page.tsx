9670e4a3a2b73b9e7b36e6e1d4f7912633d735ef
Eddie Shin <eshinhw@gmail.com>
2023-04-03 23:34:22 -0400

    updated login page

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,39 +1,46 @@
-import React from "react";
-import { FaGoogle } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaApple } from "react-icons/fa";
+import { IM_Fell_French_Canon_SC } from "next/font/google";
+import GoogleIcon from "../../../../public/images/google.png";
+import FacebookIcon from "../../../../public/images/facebook.png";
+import Image from "next/image";
 
+const fell_french = IM_Fell_French_Canon_SC({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+//  w-[30rem]
 export default function LoginPage() {
   return (
     <>
-      <div className="flex flex-col md:mx-[20rem] lg:mx-[40rem]">
-        <h1 className="text-3xl my-3 text-center">Log in or create an account</h1>
-        <h3 className="">Email Address</h3>
-        <input type="text" className="border-solid border-black border-2 p-3 my-5" />
-        <button className="bg-black text-white p-3">Continue</button>
-        <p className="text-center mt-2">or</p>
-        <p className="text-center">
-          By continuing, you agree to the updated Terms of Sale, Terms of Service, and Privacy
-          Policy.
-        </p>
-        <button className="border-solid border-2 border-black my-3">
-          <div className="flex flex-row items-center justify-center gap-4 my-4">
-            <FaGoogle />
-            Continue with Google
-          </div>
-        </button>
-        <button className="border-solid border-2 border-black my-3">
-          <div className="flex flex-row items-center justify-center gap-4 my-4">
-            <FaFacebook />
-            Continue with Facebook
-          </div>
-        </button>
-        <button className="border-solid border-2 border-black my-3">
-          <div className="flex flex-row items-center justify-center gap-4 my-4">
-            <FaApple />
-            Continue with Apple
-          </div>
-        </button>
+      <div className="flex flex-col justify-center items-center min-w-[25rem] mx-10">
+        <header className="text-center text-2xl my-5">
+          <h1 className={fell_french.className}>The Yonge Street Journal</h1>
+        </header>
+        <div className="flex flex-col">
+          <h1 className="text-xl my-3 text-center">Log in or create an account</h1>
+          <h3 className="text-sm">Email Address</h3>
+          <input type="text" className="border-solid border-black border-2 p-3 mb-5 mt-2" />
+          <button className="bg-black text-white p-3">Continue</button>
+          <p className="text-center mt-2">or</p>
+          <p className="text-center">
+            By continuing, you agree to the updated Terms of Sale, Terms of Service, and Privacy
+            Policy.
+          </p>
+          <button className="border-solid border-2 border-black my-3">
+            <div className="flex flex-row items-center justify-center gap-4 my-4">
+              {/* <FaGoogle /> */}
+              <Image src={GoogleIcon} alt="google" className="w-5" />
+              Continue with Google
+            </div>
+          </button>
+          <button className="border-solid border-2 border-black my-3">
+            <div className="flex flex-row items-center justify-center gap-4 my-4">
+              {/* <FaFacebook /> */}
+              <Image src={FacebookIcon} alt="facebook" className="w-5" />
+              Continue with Facebook
+            </div>
+          </button>
+        </div>
       </div>
     </>
   );
