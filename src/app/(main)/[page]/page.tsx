699a3061c386a250633eba4cb87e76ceb1bd48c0
Eddie Shin <eshinhw@gmail.com>
2023-04-09 12:53:52 -0400

    used external utils to convert first char uppercase

--- src/app/(main)/[page]/page.tsx
+++ src/app/(main)/[page]/page.tsx
@@ -2,6 +2,7 @@ import ArticleGrid from "@/app/components/article-grid";
 import SectionTitle from "@/app/components/section-title";
 import { getArticles, getArticlesByCategory } from "@/app/services/articles";
 import { getSectionData } from "@/app/services/sections";
+import { FirstCharUpperCase } from "@/app/services/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -31,7 +32,7 @@ export default async function page({ params }: Props) {
   const articles = await getArticlesByCategory(params.page);
   return (
     <section className="my-4 lg:mx-20 xl:mx-40">
-      <SectionTitle title={params.page.charAt(0).toUpperCase() + params.page.slice(1)} />
+      <SectionTitle title={FirstCharUpperCase(params.page)} />
       <ArticleGrid articles={articles} />
     </section>
   );
