62e212f365a85330eee3a7f7d6a5facc567e1214
Eddie Shin <eshinhw@gmail.com>
2023-04-09 01:23:44 -0400

    updated stylings

--- src/app/components/article-card.tsx
+++ src/app/components/article-card.tsx
@@ -17,7 +17,7 @@ export default function ArticleCard({
         <Image src={`/images/thumbnails/${path}`} alt="dp" width={300} height={300} className="w-full max-h-40 overflow-none object-fill"/>
         <div className="flex flex-col items-center p-4">
           <time className="self-end text-gray-700">{date.toString()}</time>
-          <h1 className="font-bold text-xl my-2">{title}</h1>
+          <h1 className="font-bold text-xl my-2 w-full truncate">{title}</h1>
           <p className="w-full text-center truncate">{description}</p>
           <span className="text-sm rounded-lg bg-sky-300 px-2 my-2">{category}</span>
         </div>
--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -7,7 +7,7 @@ export default async function CarouselArticles() {
   const articles = await getLocalArticles();
   return (
     <section className="lg:mx-20 xl:mx-40">
-      <h2 className="text-2xl text-bold my-5">You May Like</h2>
+      <h2 className="text-2xl font-bold my-5">You May Like</h2>
       <MultiCarousel>
         {articles.map((article, idx) => (
           <ArticleCard key={idx} article={article} />
