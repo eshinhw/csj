d7271b64cfccd24224879c54902b737b23928d13
Eddie Shin <eshinhw@gmail.com>
2023-04-09 11:17:17 -0400

    implemented dynamic category header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import "../globals.css";
 import { Work_Sans } from "next/font/google";
+import { getLocalArticles } from "../services/articles";
 
 const work_sans = Work_Sans({
   subsets: ["latin"],
@@ -15,9 +16,9 @@ export const metadata = {
   description: "Fair and Balanced News in Canada",
 };
 
-const header_sections = ["World", "Canada", "Food", "Health", "Realty", "Opinion"];
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const articles = await getLocalArticles();
+  const categories = [...new Set(articles.map(article => article.category))].sort()
   return (
     <html lang="en" className={work_sans.className}>
       <body>
@@ -25,9 +26,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Laptop: Sections */}
         <div className="mx-2 lg:mx-20 xl:mx-40">
           <div className="hidden lg:flex flex-row gap-8 justify-center text-xs my-3">
-            {header_sections.map((section, idx) => (
+            {categories.map((section, idx) => (
               <Link key={idx} href={section.toLowerCase()} className="cursor-pointer">
-                {section}
+                {section.toUpperCase()}
               </Link>
             ))}
           </div>
