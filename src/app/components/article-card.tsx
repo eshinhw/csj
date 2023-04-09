db8f37e60fc33d0d2f194c4a0995782222cb1ddf
Eddie Shin <eshinhw@gmail.com>
2023-04-09 11:17:31 -0400

    updated article-card comp

--- src/app/components/article-card.tsx
+++ src/app/components/article-card.tsx
@@ -14,12 +14,12 @@ export default function ArticleCard({
     // make a card component with tailwind css
     <article className="rounded-md overflow-hidden shadow-md hover:shadow-2xl">
       <Link href={`/articles/${id}`}>
-        <Image src={`/images/thumbnails/${path}`} alt="dp" width={300} height={300} className="w-full max-h-40 overflow-none object-fill"/>
+        <Image src={`/images/thumbnails/${path}`} alt="dp" width={300} height={300} className="w-full max-h-48 object-fill"/>
         <div className="flex flex-col items-center p-4">
           <time className="self-end text-gray-700">{date.toString()}</time>
           <h1 className="font-bold text-xl my-2 w-full truncate">{title}</h1>
           <p className="w-full text-center truncate">{description}</p>
-          <span className="text-sm rounded-lg bg-sky-300 px-2 my-2">{category}</span>
+          <span className="text-sm rounded-lg bg-sky-300 px-2 my-2">{category.toUpperCase()}</span>
         </div>
       </Link>
     </article>
