365800a17fb815e9dfbe6336cb4eb1882d1508af
Eddie Shin <eshinhw@gmail.com>
2023-04-10 12:31:00 -0400

    updated backend services

--- src/app/services/articles.ts
+++ src/app/services/articles.ts
@@ -1,87 +1,46 @@
 import { readFile } from "fs/promises";
 import path from "path";
 
-const sections = ["canada", "beauty", "realty"];
-
-export async function getArticles(section: string) {
-  // switch (section) {
-  //   case "canada":
-  //     res = await fetch(
-  //       `https://api.nytimes.com/svc/topstories/v2/us.json?api-key=${process.env.NYT_API}`
-  //     );
-  //   case "beauty":
-  //     res = await fetch(
-  //       `https://api.nytimes.com/svc/topstories/v2/health.json?api-key=${process.env.NYT_API}`
-  //     );
-  //   case "realty":
-  //     res = await fetch(
-  //       `https://api.nytimes.com/svc/topstories/v2/realestate.json?api-key=${process.env.NYT_API}`
-  //     );
-  //   default:
-  // }
-
-  if (section === "canada") {
-    section = "us";
-  } else if (section === "realty") {
-    section = "realestate";
-  }
-
-  const res = await fetch(
-    `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.NYT_API}`
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-export async function getTopStories(topic: string) {
-  const res = await fetch(
-    `https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${process.env.NYT_API}`
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 export type Article = {
   id: number;
   title: string;
   description: string;
   date: Date;
   category: string;
-  path: string,
+  image: string;
+  articleName: string;
   featured: boolean;
 };
 
+export type ArticleData = Article & { content: string };
+
 export function getFeaturedArticles() {
-  return getLocalArticles().then((articles) => articles.filter(article => article.featured))
+  return getLocalArticles().then((articles) => articles.filter((article) => article.featured));
 }
 
 export function getNonFeaturedArticles() {
-  return getLocalArticles().then((articles) => articles.filter(article => !article.featured))
+  return getLocalArticles().then((articles) => articles.filter((article) => !article.featured));
 }
 
 export function getArticlesByCategory(category: string) {
-  return getLocalArticles().then((articles) => articles.filter(article => article.category === category))
+  return getLocalArticles().then((articles) =>
+    articles.filter((article) => article.category === category)
+  );
 }
 
 export async function getLocalArticles() {
   const filePath = path.join(process.cwd(), "data", "articles.json");
   return readFile(filePath, "utf-8")
-    .then<Article[]>(data => JSON.parse(data))
+    .then<Article[]>((data) => JSON.parse(data))
     .then((articles) => articles.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getArticleData(articleName: string): Promise<ArticleData> {
+  const filePath = path.join(process.cwd(), "data", "articles", `${articleName}.md`);
+  console.log(filePath)
+  const metadata = await getLocalArticles().then(articles => articles.find(article => article.articleName === articleName));
+  // if (!metadata) throw new Error(`${articleName} DNE.`)
+
+  const content = await readFile(filePath, 'utf-8');
+  return {...metadata, content};
 }
