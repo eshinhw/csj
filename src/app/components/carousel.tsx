6afb3819f191f10b4250976d3e0d099d9b14717d
Eddie Shin <eshinhw@gmail.com>
2023-04-10 21:08:16 -0400

    updated

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { getLocalArticles } from "../services/articles";
 import ArticleCard from "./article-card";
 import MultiCarousel from "./multi-carousel";
+import SectionTitle from "./section-title";
 
 export default async function CarouselArticles() {
   const articles = await getLocalArticles();
   return (
     <section className="lg:mx-20 xl:mx-40">
-      <h2 className="text-2xl font-bold my-5">You May Like</h2>
+      <SectionTitle title="Trending" />
       <MultiCarousel>
         {articles.map((article, idx) => (
           <ArticleCard key={idx} article={article} />
--- src/app/components/featured-articles.tsx
+++ src/app/components/featured-articles.tsx
@@ -1,14 +1,14 @@
-import { getLocalArticles } from "@/app/services/articles";
+import { getFeaturedArticles } from "@/app/services/articles";
 import React from "react";
 import ArticleGrid from "./article-grid";
 import SectionTitle from "./section-title";
 
 export default async function FeaturedArticles() {
-  const featured = await getLocalArticles();
+  const featured = await getFeaturedArticles();
 
   return (
     <div className="mx-2 lg:mx-20 xl:mx-40">
-      <SectionTitle title="Featured Articles" />
+      <SectionTitle title="Featured" />
       <ArticleGrid articles={featured} />
     </div>
   );
