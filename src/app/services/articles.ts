e821a8ad4b300e5cb1e3d63ba6700868213bc407
Eddie Shin <eshinhw@gmail.com>
2023-10-25 00:06:33 -0400

    Fix type error for API key

--- src/app/services/articles.ts
+++ src/app/services/articles.ts
@@ -6,7 +6,7 @@ export default async function getTopHeadlines() {
   let url = new URL("https://newsapi.org/v2/top-headlines");
   url.searchParams.append("country", "ca");
   url.searchParams.append("sortBy", "popularity");
-  url.searchParams.append("apiKey", process.env.NEXT_PUBLIC_NEWSAPI);
+  url.searchParams.append("apiKey", process.env.NEXT_PUBLIC_NEWSAPI!);
   const res = await fetch(url);
   if (!res.ok) throw new Error("Failed to fetch data");
   return res.json();
@@ -21,7 +21,7 @@ export async function getArticlesByCategory(category: string) {
   let url = new URL("https://newsapi.org/v2/everything");
   url.searchParams.append("q", category);
   url.searchParams.append("sortBy", "popularity");
-  url.searchParams.append("apiKey", process.env.NEXT_PUBLIC_NEWSAPI);
+  url.searchParams.append("apiKey", process.env.NEXT_PUBLIC_NEWSAPI!);
   const res = await fetch(url);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
